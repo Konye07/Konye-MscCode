442a0cf6fdd854ca32346306265f925b6a42bf6f
Mate Konye <mate.konye@gmail.com>
2025-03-29 22:59:52 +0100

    Commiting word stats update and own collection python code

--- preprocessing_train_indep/batch2_word_stats.py
+++ preprocessing_train_indep/batch2_word_stats.py
@@ -1,6 +1,25 @@
-import pandas as pd
-import numpy as np
-
+import pandas as pd # type: ignore
+import re
+import matplotlib.pyplot as plt # type: ignore
+import ast
+import string
+from langdetect import detect # type: ignore
+from collections import Counter
+import numpy as np # type: ignore
+import spacy # type: ignore
+from nltk.tokenize import sent_tokenize, word_tokenize # type: ignore
+import inflect # type: ignore
+from nltk import pos_tag # type: ignore
+from nltk.stem import WordNetLemmatizer # type: ignore
+from nltk.corpus import wordnet # type: ignore
+from nltk.stem import PorterStemmer # type: ignore
+import numpy as np # type: ignore
+from tensorflow.keras.preprocessing.text import Tokenizer # type: ignore
+from tensorflow.keras.preprocessing.sequence import pad_sequences # type: ignore
+from konye_m_packages import __all__ # type: ignore
+from konye_m_packages import number_preproc, lemmat_processing, prepare_for_modeling_with_glove # type: ignore
+import pickle
+import nltk # type: ignore
 
 train_df1 = pd.read_csv("d:/Egyetem/01Ma_Survey/Szakdolgozat/kod/Konye-MscCode/preprocessing_train_indep/first_method/train_elso.csv")
 train_df2 = pd.read_csv("d:/Egyetem/01Ma_Survey/Szakdolgozat/kod/Konye-MscCode/preprocessing_train_indep/second_method/train_masodik.csv")
---
+++ preprocessing_train_indep/corpus_sajat_prepare.py
@@ -0,0 +1,108 @@
+from konye_m_packages import __all__ # type: ignore
+from konye_m_packages import analyze_text_column, clean_dataset # type: ignore
+import pandas as pd # type: ignore
+from sklearn.utils import resample # type: ignore
+from sklearn.model_selection import train_test_split # type: ignore
+from langdetect import detect # type: ignore
+import re
+import nltk # type: ignore
+nltk.download('words')
+from nltk.corpus import words # type: ignore
+import pandas as pd # type: ignore
+import re
+import matplotlib.pyplot as plt # type: ignore
+import ast
+import string
+from langdetect import detect # type: ignore
+from collections import Counter
+import numpy as np # type: ignore
+import spacy # type: ignore
+from nltk.tokenize import sent_tokenize, word_tokenize # type: ignore
+import inflect # type: ignore
+from nltk import pos_tag # type: ignore
+from nltk.stem import WordNetLemmatizer # type: ignore
+from nltk.corpus import wordnet # type: ignore
+from nltk.stem import PorterStemmer # type: ignore
+import numpy as np # type: ignore
+from tensorflow.keras.preprocessing.text import Tokenizer # type: ignore
+from tensorflow.keras.preprocessing.sequence import pad_sequences # type: ignore
+from konye_m_packages import __all__ # type: ignore
+from konye_m_packages import spacy_preproc,lemmat_processing, prepare_for_modeling_with_glove # type: ignore
+import pickle
+import nltk # type: ignore
+
+
+##### Saját, 40 összegyűjtött cikk #####
+
+sajat_gyujt = pd.read_csv("d:/Egyetem/01Ma_Survey/Szakdolgozat/kod/Konye-MscCode/preprocessing_train_indep/databases/sajat_gyujtes.csv")
+
+# Mennyi sora van
+print(f"Eredeti sorok száma: {len(sajat_gyujt)}")
+
+analyze_text_column(sajat_gyujt, "fake_news")
+fake_news = sajat_gyujt[['label', 'text', 'text_length']]
+
+sajat_gyujt = clean_dataset(sajat_gyujt)
+
+sajat_gyujt.to_csv("d:/Egyetem/01Ma_Survey/Szakdolgozat/kod/Konye-MscCode/preprocessing_train_indep/databases/prepareed_sajat.csv", index=False)
+
+
+sajat_df = pd.read_csv("d:/Egyetem/01Ma_Survey/Szakdolgozat/kod/Konye-MscCode/preprocessing_train_indep/databases/prepareed_sajat.csv")
+
+# Legjobban alakító modell lefuttatása erre is
+
+sajat_df = spacy_preproc(sajat_df, text_column='text', new_column='batch1')
+
+print("SpaCy preprocessing pipa")
+
+print(sajat_df.head())
+print(sajat_df.head())
+
+sajat_df2 = lemmat_processing(sajat_df, text_column='batch1', new_column='batch2')
+
+print("Lemmatizálás rész pipa")
+
+print(sajat_df2.head())
+
+sajat_df2.to_csv("d:/Egyetem/01Ma_Survey/Szakdolgozat/kod/Konye-MscCode/preprocessing_train_indep/sixth_owncollect/own_hatodik.csv", index=False)
+
+print("Saját cikkek imentve")
+print(sajat_df2.value_counts("label"))
+
+
+### Model preparáló function ###
+sajat_df2 = pd.read_csv("d:/Egyetem/01Ma_Survey/Szakdolgozat/kod/Konye-MscCode/preprocessing_train_indep/sixth_owncollect/own_hatodik.csv")
+
+# GloVe fájl elérési útja
+glove_file = 'd:/Egyetem/01Ma_Survey/Szakdolgozat/kod/Konye-MscCode/preprocessing_train_indep/databases/glove.6B.300d.txt'
+
+# Max szókincs és szekvencia hossz
+MAX_VOCAB_SIZE = 25000
+MAX_LENGTH = 600
+EMBEDDING_DIM = 300  # GloVe 300d
+
+# Szövegek előkészítése modellezéshez
+tokenized_sajat = sajat_df2['batch2'].tolist()
+
+
+# Tokenizer betöltése
+with open('d:/Egyetem/01Ma_Survey/Szakdolgozat/kod/Konye-MscCode/preprocessing_train_indep/third_method/tokenizer.pkl', 'rb') as f:
+    tokenizer = pickle.load(f)
+
+# Embedding mátrix betöltése
+embedding_matrix = np.load('d:/Egyetem/01Ma_Survey/Szakdolgozat/kod/Konye-MscCode/preprocessing_train_indep/third_method/embedding_matrix.npy')
+
+# Szövegek padding-elése a betöltött tokenizer-rel
+padded_sajat, _, _ = prepare_for_modeling_with_glove(
+    tokenized_sajat,
+    glove_file=None,  # Nem kell újra betölteni a GloVe-ot
+    tokenizer=tokenizer,
+    fit_tokenizer=False,
+    max_vocab_size=MAX_VOCAB_SIZE,
+    max_length=MAX_LENGTH,
+    embedding_dim=EMBEDDING_DIM
+)
+
+# Mentés
+np.save('d:/Egyetem/01Ma_Survey/Szakdolgozat/kod/Konye-MscCode/preprocessing_train_indep/sixth_owncollect/padded_sajat.npy', padded_sajat)
+print("Saját szövegek előkészítve és elmentve.")
